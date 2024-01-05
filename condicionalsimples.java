5fc08a17888fb269053c5947c2567868d52fc9fe
CRUZHENRIQUEDEV <Henriquecruz.dev@GMAIL.COM>
2024-01-05 10:11:07 -0300

    Novos exercícios

---
+++ condicionalsimples.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {
+      int nota = 2;
+
+      if(nota >= 5){
+        System.out.println("Aprovado");
+        System.out.println("parabéns");
+        
+      }
+    
+  }
+}
---
+++ sequencial.java
@@ -0,0 +1,14 @@
+class sequencial{
+  public static void main(String[] args) {
+      double base, altura, area;
+
+      base = 10.0;
+      altura = 8.0;
+
+      area = (base * altura) / 2;
+
+      System.out.println(area);
+
+    
+  }
+}
